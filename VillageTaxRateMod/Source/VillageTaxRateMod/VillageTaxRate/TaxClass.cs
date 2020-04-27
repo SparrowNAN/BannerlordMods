f04c572461d5e93bfc2db66ad6479698876e3259
sparrow <chwangyanan@gmail.com>
2020-04-27 08:31:30 +0800

    avillageTaxRate 1.0

--- VillageTaxRateMod/Source/VillageTaxRateMod/VillageTaxRate/TaxClass.cs
+++ VillageTaxRateMod/Source/VillageTaxRateMod/VillageTaxRate/TaxClass.cs
@@ -1,32 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.GameComponents;
-using TaleWorlds.CampaignSystem.ViewModelCollection.ClanManagement;
-using TaleWorlds.CampaignSystem.ViewModelCollection.ClanManagement.Categories;
 using TaleWorlds.Core;
-using TaleWorlds.Library;
 using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
-using SandBox.GauntletUI;
 
 namespace VillageTaxRate
 {
-    public class TaxEvent:CampaignBehaviorBase
-    {
-        public override void RegisterEvents() {   
-            CampaignEvents.OnClanDestroyedEvent.AddNonSerializedListener(this, clan => {
-                String clanName = clan.Name.ToString();
-                InformationManager.DisplayMessage(new InformationMessage("The " + clanName + " was destroyed!"));
-            });
-        }
-
-        public override void SyncData(IDataStore dataStore)
-        {
-            
-        }
-    }
-
     public class TaxLoader : MBSubModuleBase
     {
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
@@ -47,8 +27,47 @@ namespace VillageTaxRate
         public override void CalculateClanIncome(Clan clan, ref ExplainedNumber goldChange, bool applyWithdrawals = false)
         {
             base.CalculateClanIncome(clan, ref goldChange, applyWithdrawals);
-            TextObject description = new TextObject("外星人支持", (Dictionary<string, TextObject>) null);
-            goldChange.Add((float) 100000, description);
+
+            float exemptRate = 0.0f;
+            // 每个村庄的减免税收计算
+            foreach (Village village in clan.Villages)
+            {
+                exemptRate += CalculateVillageExemptFromTax(clan, village);
+            }
+            TextObject description = new TextObject("赋税减免", (Dictionary<string, TextObject>) null);
+            goldChange.Add((float) -exemptRate, description);
+        }
+
+        private float CalculateVillageExemptFromTax(Clan clan, Village village)
+        {
+            int baseTax = village.VillageState == Village.VillageStates.Looted || village.VillageState == Village.VillageStates.BeingRaided ? 0 : (int) ((double) village.TradeTaxAccumulated / (double) this.RevenueSmoothenFraction());
+            float tax = baseTax;
+            if (clan.Kingdom != null && clan.Kingdom.ActivePolicies.Contains(DefaultPolicies.LandTax))
+                tax = tax * 0.95f;
+            float exemptRate = ExemptLevelRate(village.Hearth);
+            return tax * exemptRate;
+        }
+
+        private float ExemptLevelRate(float health)
+        {
+            if (health <= 100)
+            {
+                return 0.9f;
+            } else if (health <= 200)
+            {
+                return 0.7f;
+            }
+            else if (health <= 500)
+            {
+                return 0.5f;
+            } else if (health <= 1000)
+            {
+                return 0.3f;
+            }
+            else
+            {
+                return 0.0f;
+            }
         }
     }
 
@@ -57,35 +76,30 @@ namespace VillageTaxRate
         public override float CalculateHearthChange(Village village, StatExplainer explanation = null)
         {
             float change = base.CalculateHearthChange(village, explanation);
-            return RichHearthChange(change);
+            change = change * HealthIncrementRate(village.Hearth);
+            return change;
         }
-
-        private float RichHearthChange(float change)
+        
+        private float HealthIncrementRate(float health)
         {
-            return change * 10;
-        }
-    }
-
-    public class TaxUI : GauntletClanScreen
-    {
-        pub
-    }
-    
-    public class TaxViewModel : ClanFiefsVM
-    {
-        public TaxViewModel(Action onRefresh) : base(onRefresh)
-        {
-       
-        }
-
-        [DataSourceProperty]
-        public bool CanChangeTaxRateOfCurrentFief
-        {
-            get
+            if (health <= 100)
             {
-                return !base.CanChangeGovernorOfCurrentFief;
+                return 5f;
+            } else if (health <= 200)
+            {
+                return 3f;
+            }
+            else if (health <= 500)
+            {
+                return 2f;
+            } else if (health <= 1000)
+            {
+                return 1.5f;
+            }
+            else
+            {
+                return 1f;
             }
         }
     }
-
 }
