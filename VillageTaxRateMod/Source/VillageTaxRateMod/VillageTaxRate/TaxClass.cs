9c231d43736c684a6385875b7fc177c1fa034ee0
sparrow <chwangyanan@gmail.com>
2020-05-05 18:58:22 +0800

    afix:mod interupt

--- VillageTaxRateMod/Source/VillageTaxRateMod/VillageTaxRate/TaxClass.cs
+++ VillageTaxRateMod/Source/VillageTaxRateMod/VillageTaxRate/TaxClass.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections.Generic;
-using HarmonyLib;
+﻿using HarmonyLib;
 using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.SandBox.GameComponents;
 using TaleWorlds.Core;
-using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
 using VillageTaxRate.calculate;
 using VillageTaxRate.Models;
@@ -13,20 +10,17 @@ namespace VillageTaxRate
     
     public class TaxLoader : MBSubModuleBase
     {
-        protected override void OnSubModuleLoad()
-        {
-            base.OnSubModuleLoad();
-            new Harmony("fun.wangyanan.patch.villagerate").PatchAll();
-        }
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
         {
             if(game.GameType is Campaign) {
                 //The current game is a campaign
                 CampaignGameStarter campaignStarter = (CampaignGameStarter) gameStarterObject;
-                campaignStarter.AddModel(new TaxModel());
-                campaignStarter.AddModel(new TaxHealthModel());
                 campaignStarter.AddBehavior(new CustomSaveBehavior());
-                base.OnSubModuleLoad();
+                Harmony harmony = new Harmony("fun.wangyanan.patch.villagerate");
+                if (harmony.GetPatchedMethods().IsEmpty())
+                {
+                    new Harmony("fun.wangyanan.patch.villagerate").PatchAll();
+                }
             }
         }
 
@@ -37,53 +31,10 @@ namespace VillageTaxRate
             if (VillageTaxRateMemory._villageRateDictionary != null)
             {
                 VillageTaxRateMemory._villageRateDictionary.Clear();
-                // new Harmony("fun.wangyanan.patch.villagerate").UnpatchAll();
             }
         }
     }
 
-    public class TaxModel : DefaultClanFinanceModel
-    {
-        public override void CalculateClanIncome(Clan clan, ref ExplainedNumber goldChange, bool applyWithdrawals = false)
-        {
-            base.CalculateClanIncome(clan, ref goldChange, applyWithdrawals);
-
-            float exemptRate = 0.0f;
-            // 每个村庄的减免税收计算
-            foreach (Village village in clan.Villages)
-            {
-                var reduce = CalculateVillageExemptFromTax(clan, village);
-                TextObject description = new TextObject($"{village.Name.ToString()} - 赋税减免", (Dictionary<string, TextObject>) null);
-                goldChange.Add((float) -reduce, description);
-            }
-        }
-
-        private float CalculateVillageExemptFromTax(Clan clan, Village village)
-        {
-            int baseTax = village.VillageState == Village.VillageStates.Looted || village.VillageState == Village.VillageStates.BeingRaided ? 0 : (int) ((double) village.TradeTaxAccumulated / (double) this.RevenueSmoothenFraction());
-            float tax = baseTax;
-            if (clan.Kingdom != null && clan.Kingdom.ActivePolicies.Contains(DefaultPolicies.LandTax))
-                tax = tax * 0.95f;
-            float reduceRate = VillageTaxRateMemory.GetReduceCoinRate(village);
-            return tax * reduceRate;
-        }
-    }
-
-    public class TaxHealthModel : DefaultSettlementProsperityModel
-    {
-        public override float CalculateHearthChange(Village village, StatExplainer explanation = null)
-        {
-            float change = base.CalculateHearthChange(village, explanation);
-            // 户数可能减，这里还没有相关的处理逻辑
-            if (change > 0)
-            {
-                float reduceInfluenceRate = VillageTaxRateMemory.CalculateAddHealthRate(village);
-                change = change * reduceInfluenceRate;
-            }
-            return change;
-        }
-    }
-    
     public class CustomSaveBehavior : CampaignBehaviorBase
     {
         private static VillageModel _customDataMap = new VillageModel();
---
+++ VillageTaxRateMod/Source/VillageTaxRateMod/VillageTaxRate/VillageRateCalculatePatch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.ViewModelCollection.ClanManagement;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection.ClanManagement.Categories;
+using VillageTaxRate.ViewModel;
+using TaleWorlds.Localization;
+using System.Collections.Generic;
+
+using VillageTaxRate.calculate;
+
+namespace VillageTaxRate.Patchs
+{
+    [HarmonyPatch(typeof(ClanVM), "set_ClanFiefs")]
+    public class VillageRatePatch
+    {
+        public static void Prefix(ref ClanFiefsVM value)
+        {
+            Traverse traverse = Traverse.Create((object) value);
+            ClanFiefsWithVillageRateVM vm = new ClanFiefsWithVillageRateVM(traverse.Field<Action>("_onRefresh").Value);
+            value = vm;
+        }
+    }
+    
+    [HarmonyPatch(typeof(DefaultClanFinanceModel), "CalculateClanIncome")]
+    public class VillageRateCalculatePatch
+    {
+        public static void Postfix(ref Clan clan, ref ExplainedNumber goldChange)
+        {
+            float exemptRate = 0.0f;
+            // 每个村庄的减免税收计算
+            foreach (Village village in clan.Villages)
+            {
+                var reduce = CalculateVillageExemptFromTax(clan, village);
+                TextObject description = new TextObject($"{village.Name.ToString()} - 赋税减免", (Dictionary<string, TextObject>) null);
+                goldChange.Add((float) -reduce, description);
+            }
+        }
+        
+        private static float CalculateVillageExemptFromTax(Clan clan, Village village)
+        {
+            int baseTax = village.VillageState == Village.VillageStates.Looted || village.VillageState == Village.VillageStates.BeingRaided ? 0 : (int) ((double) village.TradeTaxAccumulated / (double) 5f);
+            float tax = baseTax;
+            if (clan.Kingdom != null && clan.Kingdom.ActivePolicies.Contains(DefaultPolicies.LandTax))
+                tax = tax * 0.95f;
+            float reduceRate = VillageTaxRateMemory.GetReduceCoinRate(village);
+            return tax * reduceRate;
+        }
+    }
+    
+    [HarmonyPatch(typeof(DefaultSettlementProsperityModel), "CalculateHearthChange")]
+    public class VillageRateCalculateHealthPatch
+    {
+        public static void Postfix(ref Village village,ref float __result)
+        {
+            if (__result > 0)
+            {
+                float reduceInfluenceRate = VillageTaxRateMemory.CalculateAddHealthRate(village);
+                __result = __result * reduceInfluenceRate;
+            }
+        }
+    }
+}
