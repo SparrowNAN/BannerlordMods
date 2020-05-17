4621da58a6765ae03b036e7802c8604f5d8e0982
sparrow <chwangyanan@gmail.com>
2020-05-17 11:33:59 +0800

    a思源宋体MOD

---
+++ UseSourceHanSerifSCFont/Source/UseSourceHanSerifSCFont/UseSourceHanSerifSCFont/Class1.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace UseSourceHanSerifSCFont
+{
+    public class UseSCFont : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Harmony harmony = new Harmony("fun.wangyanan.patch.use.sourcehanserifc.fonts");
+            if (harmony.GetPatchedMethods().IsEmpty())
+            {
+                new Harmony("fun.wangyanan.patch.use.sourcehanserifc.fonts").PatchAll();
+            }
+        }
+    }
+}
---
+++ UseSourceHanSerifSCFont/Source/UseSourceHanSerifSCFont/UseSourceHanSerifSCFont/FontPatch.cs
@@ -0,0 +1,44 @@
+﻿﻿﻿using HarmonyLib;
+using TaleWorlds.GauntletUI;
+using TaleWorlds.Engine.GauntletUI;
+using TaleWorlds.TwoDimension;
+
+namespace CustomChineseFont
+{
+    [HarmonyPatch(typeof(FontFactory), "OnLanguageChange")]
+    public class DefaultFontPatch
+    {
+        public static void Postfix(ref string newLanguageCode)
+        {
+            if (newLanguageCode.Equals("简体中文"))
+            {
+                UIResourceManager.FontFactory.DefaultFont = UIResourceManager.FontFactory.GetFont("SourceHanSerifSC-Regular");
+            }
+        }
+    }
+    
+    [HarmonyPatch(typeof(FontFactory), "GetFont")]
+    public class GetFontPatch
+    {
+        public static void Prefix(ref string fontName)
+        {
+            if (fontName.Equals("simkai") && (UIResourceManager.FontFactory.CurrentLangageID.Equals("简体中文")))
+            {
+                fontName = "SourceHanSerifSC-Regular";
+            }
+        }
+    }
+    
+    [HarmonyPatch(typeof(FontFactory), "GetMappedFontForLocalization")]
+    public class GetMappedFontPatch
+    {
+        public static void Postfix(ref string englishFontName, ref Font __result)
+        {
+            if (__result.Name.Equals("simkai") && (UIResourceManager.FontFactory.CurrentLangageID.Equals("简体中文")))
+            {
+                __result = UIResourceManager.FontFactory.GetFont("SourceHanSerifSC-Regular");
+            }
+        }
+    }
+    
+}
